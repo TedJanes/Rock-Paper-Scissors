8dda92561581e6f91fddaddc3b31b29837e79e79
Thaddeus Halle <Thaddeusjhalle@gmail.com>
2023-03-10 19:45:43 -0500

    added computer choice

--- java.js
+++ java.js
@@ -1,19 +1,28 @@
-// collects user's choice of rock, paper or scissors but is cas sensitive on the answer
+let userAnswer = question()
+let compAnswer = compChoice()
+//rock =0, paper = 1, scissor = 2
 function question() {
-    let response
+    let userAnswer = ''
     let choice = prompt('Rock, Paper, or Scissors?')
-    if ('rock') {
-    response = 'You have chosen rock'
-  } else if ('paper') {
-    response = 'You have chosen paper'
-} else if ('scissors') {
-    response = 'You have chosen Scissors'
+    choice = choice.toLowerCase()
+    if (choice === 'rock') {
+    alert('You have chosen rock')
+    answer = 0
+  } else if (choice === 'paper') {
+    alert('You have chosen paper')
+    answer = 1
+} else if (choice === 'scissors') {
+    alert('You have chosen Scissors')
+    answer = 2
     }
     else {
-    response = 'Invalid answer: please choose Rock, Paper, or Scissors.'
+    alert('Invalid answer: please choose Rock, Paper, or Scissors.')
+    question()
     } 
-    return response
+    return userAnswer
 }
-question(response)
-//I dont think choice is getting saved as an answer
-console.log(response)
+// Computer needs to choose a number between 1-3 randomly and selects it. 1-3 = r, p. or s.  computer then sends answer
+function compChoice() {
+    return Math.floor(Math.random * 2)
+}
+
