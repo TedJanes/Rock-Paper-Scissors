1d9c3cea651befcfbd9b56fdb6886007cc5b3498
Thaddeus Halle <Thaddeusjhalle@gmail.com>
2023-03-10 22:37:15 -0500

    added computer random choice

--- java.js
+++ java.js
@@ -1,19 +1,18 @@
-let userAnswer = question()
-let compAnswer = compChoice()
+duel(question(), compChoice())
 //rock =0, paper = 1, scissor = 2
 function question() {
     let userAnswer = ''
-    let choice = prompt('Rock, Paper, or Scissors?')
+    let choice = prompt('Human, I challenge you to Rock, Paper, or Scissors. What do you choose?')
     choice = choice.toLowerCase()
     if (choice === 'rock') {
     alert('You have chosen rock')
-    answer = 0
+    userAnswer = 0
   } else if (choice === 'paper') {
     alert('You have chosen paper')
-    answer = 1
+    userAnswer = 1
 } else if (choice === 'scissors') {
     alert('You have chosen Scissors')
-    answer = 2
+    userAnswer = 2
     }
     else {
     alert('Invalid answer: please choose Rock, Paper, or Scissors.')
@@ -21,8 +20,12 @@ function question() {
     } 
     return userAnswer
 }
-// Computer needs to choose a number between 1-3 randomly and selects it. 1-3 = r, p. or s.  computer then sends answer
-function compChoice() {
+function compChoice() {//+
     return Math.floor(Math.random * 2)
 }
-
+function duel(user, comp) {
+    return user > comp ? alert('I am defeated') 
+    : user == comp ? alert('Fuck a tie, we go again')//needs fixed
+    : user < comp ? alert('I am victorious'): console.log('mark has smol pp')
+    
+}
